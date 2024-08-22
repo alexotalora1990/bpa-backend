ebbfa33981c17a784fdb64f551a6f76cacb2d29f
Checho <ivansolanoquintero.lisurp@gmail.com>
2024-08-22 00:05:51 -0500

    cambios checho

--- helpers/Empleados.js
+++ helpers/Empleados.js
@@ -1,18 +1,33 @@
-import Empleado from "../models/Empleados.js"
+import Empleado from "../models/Empleados.js";
 
-    const helpersEmpleado = {
-        validarExistaIdEmpleados:async (id)=>{
-            const existe = await Empleado.findById(id)
-            if (existe==undefined){
-                throw new Error ("Id del empleado no existe")
-            }
-        },
-        validarCorreoUnico:async (correo) =>{
-            const unico = await Empleado.findOne({correo})
-            if(unico){
-                throw new Error ("Correo Existe")
-            }
+const helpersEmpleado = {
+    validarExistaIdEmpleados: async (id) => {
+        const existe = await Empleado.findById(id);
+        if (!existe) {
+            throw new Error("El ID del empleado no existe");
         }
-}
+    },
 
-export default helpersEmpleado
+    validarCorreoUnico: async (correo, { req }) => {
+        const empleado = await Empleado.findOne({ correo });
+        if (empleado && empleado._id.toString() !== req.params.id) {
+            throw new Error("El correo ya existe");
+        }
+    },
+
+    validarDocumentoUnico: async (numdocumento, { req }) => {
+        const empleado = await Empleado.findOne({ numdocumento });
+        if (empleado && empleado._id.toString() !== req.params.id) {
+            throw new Error("El documento ya existe");
+        }
+    },
+
+    validarTelefonoUnico: async (telefono, { req }) => {
+        const empleado = await Empleado.findOne({ telefono });
+        if (empleado && empleado._id.toString() !== req.params.id) {
+            throw new Error("El teléfono ya existe");
+        }
+    }
+};
+
+export default helpersEmpleado;
