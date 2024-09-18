bf34895dc42aa0c73eb211791aac0f24a2122396
Checho <ivansolanoquintero.lisurp@gmail.com>
2024-09-17 22:02:13 -0500

    semillas

--- models/Semillas.js
+++ models/Semillas.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 
 const semillaSchema = new mongoose.Schema({
-    idproveedores: { type: mongoose.Schema.Types.ObjectId, ref: 'Proveedores', required: true },
-    numFactura:{type: Number,required:true},
-    fechaCompra:{type: Date,required:true},
+    idfincas: { type: mongoose.Schema.Types.ObjectId, ref: 'Finca', required: true },
+    nombre:{type: String,required:true},
+    registroica:{type: String,required:true},
+    registroinvima:{type: String,required:true},
     fechaVencimiento:{type: Date,required:true},
     especie:{type: String,required:true},
     NumLote:{type: Number,required:true},
     origen:{type: String,required:true},
     poderGerminativo:{type: String,required:true},
-    unidadtotal:{type: Number,required:true},
-    total:{type: Number,required:true},
+    observaciones:{type: String,required:true},
+    cantidad:{type: Number,required:true},
     estado:{type:Number,default:1}
 })
+
 
 export default mongoose.model("Semilla", semillaSchema)
 
