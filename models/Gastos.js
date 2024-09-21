203351a7b6f74ba17c6d562eb837ab5861b82d6d
alexotalora1990 <alexotaloracorredor@gmal.com>
2024-09-21 10:42:31 -0500

    gastos

--- models/Gastos.js
+++ models/Gastos.js
@@ -1,12 +1,25 @@
 import mongoose from "mongoose";
 const gastoSchema = new mongoose.Schema({
+idfinca:{type:mongoose.Schema.Types.ObjectId,ref:'Finca',required:true},
     nombre:{type:String,required:true},
     fecha:{type:Date,required:true},
     numfactura:{type:String,required:true},
     descripcion:{type:String,required:true},
-    idinsumos:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"Insumo"},//aqui se trae el total de insumos
-    idsemillas:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"Semilla"},
-    idmatenimiento:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"Mantenimiento"},
+    total:{type:Number, require:true},
+    insumos:{
+        idproveedor:{type:mongoose.Schema.Types.ObjectId,ref:'Proveedor',required:false},
+        idinsumo:{type:mongoose.Schema.Types.ObjectId,ref:'Insumo',required:false},
+        unidad:{type:String,require:false},
+        totali:{type:Number, require:false},
+        cantidad:{type:Number, require:false}
+    },
+    semillas:{
+        idproveedor:{type:mongoose.Schema.Types.ObjectId,ref:'Proveedor',required:false},
+        idsemilla:{type:mongoose.Schema.Types.ObjectId,ref:'Semilla',required:false},
+        unidad:{type:String,require:false},
+        total:{type:Number, require:false},
+        cantidad:{type:Number, require:false}
+    },
     estado:{type:Number,default:1}
 })
 
